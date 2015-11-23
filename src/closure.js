9572c774a9197daf06bbe1cbf2933e7707ab3d63
Fedomn <shadowforfiend@gmail.com>
2015-11-23 19:45:10 +0800

    append closure

--- src/closure.js
+++ src/closure.js
@@ -15,7 +15,7 @@ function f1() {
 }
 
 var r = f1();
-console.log(r());
+console.log(r());//2
 
 /** 闭包用途 **/
 console.log(
@@ -25,7 +25,7 @@ console.log(
 );
 
 console.log(
-    [1,2,3].map(function (ele) {
+    [1, 2, 3].map(function (ele) {
         return ele * 2;
     })
 );
@@ -52,7 +52,36 @@ function box() {
 }
 
 var b = box();
-console.log(b.type());
+console.log(b.type());//undefined
 b.set(12);
-console.log(b.get());
-console.log(b.type());
+console.log(b.get());//12
+console.log(b.type());//number
+
+//如下：证明闭包存储的是外部变量的引用，循环体里分配的每个闭包，都存储的是i的引用，所以创建完毕后，变量i为最后的值
+function wrapElements(a) {
+    var result = [];
+    for (var i = 0, n = a.length; i < n; i++) {
+        result[i] = function () {
+            return a[i];
+        };
+    }
+    return result;
+}
+
+var wrapped = wrapElements([10, 20, 30, 40, 50]);
+var f = wrapped[0];
+console.log(f());//undefined
+
+//解决方法：使用立即调用的函数表达式，来强制创建局部作用域
+function wrapElements2(a) {
+    var result = [];
+    for (var i = 0, n = a.length; i < n; i++) {
+        (function (j) {
+            result[i] = function () {
+                return a[j];
+            }
+        }(i));
+    }
+    return result;
+}
+console.log(wrapElements2([10, 20, 30, 40, 50])[3]());
